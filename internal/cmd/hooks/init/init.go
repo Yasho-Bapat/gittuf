7d7532258b0eb3ceb0d3a227a0a4ea3e7bd2b33e
yashobapat <f20210006@goa.bits-pilani.ac.in>
2024-11-12 13:32:22 -0500

    gittuf-git: Add hooks
    Signed-off-by: yashobapat <f20210006@goa.bits-pilani.ac.in>

---
+++ internal/cmd/hooks/init/init.go
@@ -0,0 +1,40 @@
+package init
+
+import (
+	"github.com/gittuf/gittuf/experimental/gittuf"
+	"github.com/gittuf/gittuf/internal/cmd/common"
+	"github.com/gittuf/gittuf/internal/cmd/policy/persistent"
+	"github.com/spf13/cobra"
+)
+
+type options struct {
+	p              *persistent.Options
+	policyName     string
+	authorizedKeys []string
+	threshold      int
+}
+
+func (o *options) Run(cmd *cobra.Command, _ []string) error {
+	repo, err := gittuf.LoadRepository()
+	if err != nil {
+		return err
+	}
+
+	// initialize policy
+	// add rule for protecting refs/gittuf/hooks
+
+	return repo.InitializeHooks(cmd.Context())
+}
+
+func New(persistent *persistent.Options) *cobra.Command {
+	o := &options{p: persistent}
+	cmd := &cobra.Command{
+		Use:               "init",
+		Short:             "Initialize hooks ref",
+		PreRunE:           common.CheckIfSigningViableWithFlag,
+		RunE:              o.Run,
+		DisableAutoGenTag: true,
+	}
+
+	return cmd
+}
